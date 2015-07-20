c2e06ca48de9e1025115fddb9bc8049faf23e8ad
unknown <njohnson@SWF20236.ad.swfwmd.net>
2015-07-20 10:16:47 -0400

    figured out some settings

--- waterBalanceExploratory.R
+++ waterBalanceExploratory.R
@@ -1,11 +1,23 @@
 # install.packages("ggplot2")
+library(reshape2)
 setwd(file.path("L:","Hydro Eval", "Projects","MFLs","SWUCA Lakes Re-evaluations Model Group","Starr","Water Balance"))
 wbData <- read.csv(file = "lakeStarrWaterBudgetResults20150716.csv", header = TRUE)
 wbData$date <- as.Date(wbData$date, format = "%m/%d/%Y")
-lakeStarrPlot <- ggplot(wbData, aes(date,levelLakeStarrOrig)) +
-  geom_point() +
-  geom_point(aes(date, levelUFAHartFilled4Feet), color = "blue") +
-  ylab("level") +
-  xlab("date") +
-  labs(color = "Legend text") +
-  theme_bw()
+meltdf <- melt(wbData, id = "date", na.rm = TRUE)
+
+# variableName = "evapStarrUSGS_ftday"
+# ylabel = "test"
+# variablePlot(variableName, ylabel)
+
+variablePlot <- function(variableName, ylabel){
+  df <- meltdf[meltdf$variable == c(variableName),]
+  ggplot(df, aes(x = date, y = value, colour = variable, group = variable, linetype = variable)) + 
+    geom_line(size = 0.25) +
+    ylab(ylabel) +
+    theme(legend.position = "bottom",  
+    axis.text = element_text(size = 8),
+    axis.title.x = element_blank(),
+    axis.title.y = element_text(size = 8),
+    legend.text = element_text(size = 8),
+    legend.title=element_blank())
+}
